f930b416419d11f8ec84462ec89167e684e8e66e
JeniksSL <118624174+JeniksSL@users.noreply.github.com>
2023-01-03 09:07:49 +0300

    Update Main.java

--- src/Event/Main.java
+++ src/Event/Main.java
@@ -10,11 +10,38 @@ public class Main {
         /*System.out.println("Welcome to EventBY!\n");
         Scanner in = new Scanner(System.in);
         System.out.print("Do you want to continue as...\n 1.Vendor\n 2.User\n 3.Admin\n Your choice: ");
+        
         int choice = in.nextInt();
+        
         while (!(choice == 1 || choice== 2 || choice == 3)) {
             System.out.println("Incorrect entered value: " + in.nextInt() + ". Expected: 1, 2 or 3. \nYour choice:");
             in.next();
         }
+        
+        //my proposal:
+        int choice;        
+       //First need to create cyclic input with do - while. 
+        do {
+            try{
+                choice = in.nextInt(); //Throws: InputMismatchException – if the next token does not match the Integer regular expression, or is out of range
+            } catch (InputMismatchException exception) {
+                choice=0; //If thrown InputMismatchException, then choice=0;
+            }
+            //And check for incorrect input :
+            if((choice == 1 || choice == 2 || choice == 3)) {                
+                break;
+            } else {
+                System.out.println("Incorrect entered value: " //it is incorrect because you read next input: + in.nextInt()  
+                + ". Expected: 1, 2 or 3. \nYour choice:");
+            }           
+        } while (true);
+        //but better way to use: String str = in.next(); try{choice=Integer.parseInt(str)} catch(NumberFormatException e) {choice=0;}
+        //And in this case you can print incorrect input: str;
+        //System.out.println("Incorrect entered value: " + str + ". Expected: 1, 2 or 3. \nYour choice:");
+        
+        
+        
+        
         switch (choice){
             case 1: {System.out.println("You are logged in as Vendor.");}; break;
             case 2: {System.out.println("You are logged in as User.");}; break;
